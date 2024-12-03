0f998a92f1123ae1a5378269d8c881b905807bce
omthoriya <140819581+omthoriya@users.noreply.github.com>
2024-12-03 16:58:41 +0530

    Delete PHYTHON/lab-1/prog3.dart

--- PHYTHON/lab-1/prog3.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-
-void main(){
-  stdout.write("enter a fahrenheit :");
-  double fahrenheit = double.parse(stdin.readLineSync()!);
-  double celcius = ((fahrenheit-32)*5/9);
-  print("temparature in celcuius $celcius");
-}
