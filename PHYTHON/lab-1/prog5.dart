2dad3eef0041e40666b93b16a207d41f5ffd9fa6
omthoriya <140819581+omthoriya@users.noreply.github.com>
2024-12-03 16:56:09 +0530

    Delete PHYTHON/lab-1 directory

--- PHYTHON/lab-1/prog5.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-
-void main(){
-  stdout.write("enter a number in meter :");
-  int meter = int.parse(stdin.readLineSync()!);
-
-  double feet = meter * 0.304;
-  print("feet $feet");
-}
